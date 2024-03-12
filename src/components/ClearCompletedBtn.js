e84cd2ce0f25a1b24d1c894749d10c73d2edac7f
ymm <ymm2135@gmail.com>
2024-03-12 20:13:58 +0630

    mvc-todo

---
+++ src/components/ClearCompletedBtn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ClearCompletedBtn({clearCompleted}) {
+  return (
+    <div>
+        <button className="button" onClick={clearCompleted}>Clear completed</button>
+    </div>
+  )
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Todo from './Todo'
+
+export default function TodoList({todos, deleteTodo, updateTodo}) {
+  return (
+    <ul className="todo-list">
+        
+        { todos.map((todo) => (
+            <Todo todo={todo} key={todo.id} deleteTodo={deleteTodo} updateTodo={updateTodo}/>
+        ))}
+        
+    </ul>
+  )
+}
